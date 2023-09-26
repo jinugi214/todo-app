5ce75d952e1c5bd93a604411ee1b6b879c2e8fcc
정진욱 <wlsdnr214@naver.com>
2023-09-27 00:16:26 +0900

    Chore: change Setup file

--- react/todo-app/src/components/todo/TodoApp.jsx
+++ react/todo-app/src/components/todo/TodoApp.jsx
@@ -1,66 +1,35 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useParams, Link } from 'react-router-dom'
 import './TodoApp.css'
+import LogoutComponent from './LogoutComponent'
+import LoginComponent from './LoginComponent'
+import HeaderComponent from './HeaderComponent'
+import FooterComponent from './FooterComponent'
+import ListTodosComponent from './ListTodosComponent'
+import ErrorComponent from './ErrorComponent'
+import WelcomeComponent from './WelcomeComponent'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <LoginCompoent />
-            {/* <WelcomeComponent /> */}
+            <BrowserRouter>
+                <HeaderComponent />
+                <Routes>
+                    <Route path='/' element={<LoginComponent />} />
+                    <Route path='/login' element={<LoginComponent />} />
+                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
+                    <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='/logout' element={<LogoutComponent />} />
+
+                    <Route path='*' element={<ErrorComponent />} />
+                </Routes>
+                <FooterComponent />
+            </BrowserRouter>
         </div>
     )
 }
 
-function LoginCompoent() {
 
-    const [username, setUsername] = useState('jinuk')
-    const [password, setPassword] = useState('')
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-    const [showErrorMessage, setShowErrorMessage] = useState(false)
-    
 
-    function handleUsernameChange(event){
-        setUsername(event.target.value)
-    }
 
-    function handlePasswordChange(event){
-        setPassword(event.target.value)
-    }
 
-    function handleSubmit(){
-        if(username === 'jinuk' && password === 'dummy'){
-            setShowSuccessMessage(true)
-            setShowErrorMessage(false)
-        } else {
-            setShowSuccessMessage(false)
-            setShowErrorMessage(true)
-        }
-    }
-    
-    return (
-        <div className="Login">
-            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
-            {showErrorMessage && <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name:</label>
-                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-                </div>
-                <div>
-                    <button type="button" name="login" onClick={handleSubmit}>login</button>
-                </div>
-            </div>
-        </div>
-    )
-}
 
-function WelcomeComponent() {
-    return (
-        <div className="Welcome">
-            Welcome Component
-        </div>
-    )
-}
